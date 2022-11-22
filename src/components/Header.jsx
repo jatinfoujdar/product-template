8865f68b267c9fd4e1434addb30d87be61bc12e8
jatin foujdar <jatin321001@gmail.com>
2022-11-23 01:31:49 +0530

    Add cart icons

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,16 +1,18 @@
 import React from 'react'
-import { AppBar, Badge, IconButton, Toolbar, Typography  } from '@mui/material'
+import { AppBar, Badge, IconButton, Toolbar, Typography, Box  } from '@mui/material'
 import ShoppingCartIcon from "@mui/icons-material/ShoppingCartSharp"
 
 function Header() {
   return <AppBar>
     <Toolbar>
         <Typography variant='h6' color="inherit" sx={{flexGrow:1}}>Flipkart</Typography>
+        <Box sx={{display:{md:"flex"}}}>
         <IconButton size="large" aria-label="shows cart items count" color='inherit'>
          <Badge badgeContent={1}>
             <ShoppingCartIcon />
          </Badge>
         </IconButton>
+        </Box>
     </Toolbar>
   </AppBar>
 }
